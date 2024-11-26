d6e0c18888b752ca3f376f00e0a4885906f37e82
Skylar Kline <skylarkline16@gmail.com>
2024-11-25 17:19:22 -0800

    removed visible API string

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -42,7 +42,7 @@ class WeatherService {
 
   constructor() {
     this.baseURL = process.env.API_BASE_URL || "https://api.openweathermap.org";
-    this.APIKey = process.env.API_KEY || "4a197567491d61f48d2d8aaec1c2818b";
+    this.APIKey = process.env.API_KEY || "";
   }
   // TODO: Create fetchLocationData method
   private async fetchLocationData(city: string) {
@@ -131,7 +131,35 @@ class WeatherService {
       humidity
     );
   }
-  
+  // TODO: Complete buildForecastArray method
+  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+  //   const forecastArray: Weather[] = [];
+
+  //   for (const day of weatherData) {
+  //     if (day.dt !== currentWeather.date) {
+  //       const cityName = currentWeather.cityName;
+  //       const date = day.dt;
+  //       const icon = day.weather[0].icon;
+  //       const description = day.weather[0].description;
+  //       const temp = day.main.temp;
+  //       const windSpeed = day.wind.speed;
+  //       const humidity = day.main.humidity;
+
+  //       const forecastWeather = new Weather(
+  //         cityName,
+  //         date,
+  //         icon,
+  //         description,
+  //         temp,
+  //         windSpeed,
+  //         humidity
+  //       );
+
+  //       forecastArray.push(forecastWeather);
+  //     }
+  //   }
+  //   return forecastArray;
+  // }
   private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
     const forecastArray: Weather[] = [];
     const uniqueDays: Set<string> = new Set();
